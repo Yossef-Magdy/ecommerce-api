6f495c5264d89f5cdb0a251ac13796185cfe54e8
Tarek Hesham <tarekghost6@gmail.com>
2024-09-24 11:58:52 +0300

    update Coupon relation

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\Orders\Order;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Coupon extends Model
 {
@@ -19,4 +21,9 @@ class Coupon extends Model
         'discount_value',
         'expiry_date',
     ];
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
