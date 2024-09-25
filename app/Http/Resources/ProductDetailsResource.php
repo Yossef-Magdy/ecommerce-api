c138bf05582d4fd5799c05dee2d74d9cb2730c01
Tarek Hesham <tarekghost6@gmail.com>
2024-09-25 14:05:32 +0300

    Refactor Resources

--- app/Http/Resources/ProductDetailsResource.php
+++ app/Http/Resources/ProductDetailsResource.php
@@ -18,30 +18,41 @@ class ProductDetailsResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $averageRating = $this->reviews->avg('rating');
+        $averageRating = $averageRating ? round($averageRating, 1) : 0;
+
+        $attributes = $this->productOptions
+            ->load('attributeOption.attribute')
+            ->map(function ($productOption) {
+                $attributeName = $productOption->attributeOption->attribute->name;
+                $value = $productOption->attributeOption->value;
+                return [
+                    'attribute' => $attributeName,
+                    'value' => in_array($attributeName, ['stock', 'price']) ? (float)$value : $value,
+                ];
+            })
+            ->groupBy('attribute')
+            ->map(fn($options) => [
+                'attribute' => $options->first()['attribute'],
+                'options' => $options->pluck('value')->all()
+            ])
+            ->values()
+            ->all();
+
+        $cover = Str::startsWith($this->cover_image, 'http') ? $this->cover_image : asset("cover/{$this->cover_image}");
         return [
             'id' => $this->id,
             'slug' => Str::slug($this->name, '-'),
             'name' => $this->name,
             'description' => $this->description,
-            'reviews' => $this->reviews->count(),
+            'reviews_count' => $this->reviews->count(),
+            'rating' => $averageRating,
             'categories' => $this->categories,
             'sub_categories' => $this->subCateroies,
             'discount' => $this->discount,
-            'cover_image' => Str::startsWith($this->cover_image, 'http') ? $this->cover_image : asset("cover/{$this->cover_image}"),
+            'cover_image' => $cover,
             'images' => ProductImagesResource::collection($this->images),
-            'attributes' => $this->productOptions
-                ->load('attributeOption.attribute')
-                ->groupBy(fn($productOption) => $productOption->attributeOption->attribute->name)
-                ->map(fn($options, $attributeName) => [
-                    'attribute' => $attributeName,
-                    'options' => $options->map(function ($option) use ($attributeName) {
-                        return $attributeName === 'price'
-                            ? (float) $option->attributeOption->value
-                            : ($attributeName === 'stock' ? (int) $option->attributeOption->value : $option->attributeOption->value);
-                    })->all()
-                ])
-                ->values()
-                ->all(),
+            'attributes' => $attributes,
         ];
     }
 }
--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class ProductResource extends JsonResource
@@ -15,15 +16,33 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $attributes = $this->productOptions
+            ->load('attributeOption.attribute')
+            ->whereNotIn('attributeOption.attribute.name', ['price', 'size'])
+            ->map(function ($productOption) {
+                $attributeName = $productOption->attributeOption->attribute->name;
+                $value = $productOption->attributeOption->value;
+                return [
+                    'attribute' => $attributeName,
+                    'value' => $attributeName === 'stock' ? (int)$value : $value,
+                ];
+            })
+            ->groupBy('attribute')
+            ->map(fn($options) => [
+                'attribute' => $options->first()['attribute'],
+                'options' => $options->pluck('value')->all()
+            ])
+            ->values()
+            ->all();
+
         return [
             'id' => $this->id,
             'slug' => Str::slug($this->name, '-'),
             'name' => $this->name,
-            'stock' => $this->stock,
-            'color' => $this->color,
             'discount' => $this->discount,
             'cover_image' => Str::startsWith($this->cover_image, 'http') ? $this->cover_image : asset("cover/{$this->cover_image}"),
             'images' => ProductImagesResource::collection($this->images),
+            'attributes' => $attributes
         ];
     }
 }
