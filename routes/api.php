feb21927cb162bf6ba44b9173efef337f13d678a
yossef <yossefmagdy78@gmail.com>
2024-09-28 02:55:45 +0300

    update router

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
-use App\Http\Controllers\Control\UserphController;
+use App\Http\Controllers\Control\UserController;
+use App\Http\Controllers\Control\CategoryController;
+use App\Http\Controllers\Control\SubcategoryController;
+use App\Http\Controllers\Control\OrderController;
+use App\Http\Controllers\Control\CouponController;
 use App\Http\Controllers\Api\ProductReviewsController;
 use App\Http\Controllers\Api\ProductsController;
 use Illuminate\Support\Facades\Route;
@@ -22,7 +26,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::apiResource('/products', ProductsController::class)->only(['store', 'update', 'destroy']);
         Route::apiResource('/categories', CategoryController::class);
         Route::apiResource('/subcategories', SubcategoryController::class);
-        Route::apiResource('/coupons', CouponsController::class);
+        Route::apiResource('/coupons', CouponController::class);
         Route::apiResource('/orders', OrderController::class)->only(['index', 'show', 'update']);
     });
 
