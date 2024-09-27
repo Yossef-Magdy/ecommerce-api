b75f82f3ca58f6827f1ddb2904111d450e2763ee
yossef <yossefmagdy78@gmail.com>
2024-09-28 02:55:25 +0300

    add missing import

--- app/Models/Categories/Category.php
+++ app/Models/Categories/Category.php
@@ -6,6 +6,7 @@ use App\Models\Products\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Categories\Subcategory;
 
 class Category extends Model
 {
