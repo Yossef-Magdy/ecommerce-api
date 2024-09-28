67b58285900160977ab53b474097039583fcaa6e
yossef <yossefmagdy78@gmail.com>
2024-09-28 03:05:59 +0300

    update category controller

--- app/Http/Controllers/Control/CategoryController.php
+++ app/Http/Controllers/Control/CategoryController.php
@@ -4,24 +4,33 @@ namespace App\Http\Controllers\Control;
 
 use App\Http\Controllers\Controller;
 use App\Models\Categories\Category;
+use App\Http\Resources\CategoryResource;
+use App\Http\Requests\Control\StoreCategoryRequest;
+use App\Http\Requests\Control\UpdateCategoryRequest;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    function __construct()
+    {
+        $this->authorizeResource(Category::class, 'category');
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return CategoryResource::collection(Category::all());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
+        Category::create($request->validated());
+        return response()->json(['message' => 'category added successfully']);
     }
 
     /**
@@ -29,15 +38,16 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return new CategoryResource($category);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $category->update($request->validated());
+        return response()->json(['message' => 'category updated successfully']);
     }
 
     /**
@@ -45,6 +55,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response()->json(['message' => 'category deleted successfully']);
     }
 }
