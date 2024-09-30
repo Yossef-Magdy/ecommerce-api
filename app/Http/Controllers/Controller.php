c1630a3873a293347c1e6d5f6150f786b49b1952
yossef <yossefmagdy78@gmail.com>
2024-09-30 03:27:16 +0300

    update base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,9 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests;
+    protected string $modelName = "resource";
+    function createdResponse()
+    {
+        return response()->json([
+            'message' => "$this->modelName created successfully"
+        ], 200);
+    }
+    function updatedResponse()
+    {
+        return response()->json([
+            'message' => "$this->modelName updated successfully"
+        ], 200);
+    }
+    function deletedResponse()
+    {
+        return response()->json([
+            'message' => "$this->modelName deleted successfully"
+        ], 200);
+    }
 }
