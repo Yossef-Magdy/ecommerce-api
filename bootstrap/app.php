5ee4d637e1c3ebd81a04ea35c57e857558f91d20
yossef <yossefmagdy78@gmail.com>
2024-09-30 03:27:00 +0300

    add method not supported exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -30,6 +31,13 @@ return Application::configure(basePath: dirname(__DIR__))
                 return response()->json([
                     'message' => 'forbidden'
                 ], 403);
+            }
+        });
+        $exceptions->render(function (MethodNotAllowedHttpException $e, Request $request){
+            if ($request->is('api/*')) {
+                return response()->json([
+                    "message" => $e->getMessage(),
+                ], 405);
             }
         });
     })->create();
