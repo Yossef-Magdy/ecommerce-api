3fbd370f72c1ff20298a8e9b952d6510fc8c072b
yossef <yossefmagdy78@gmail.com>
2024-09-30 03:36:55 +0300

    add governorate policy

---
+++ app/Policies/GovernoratePolicy.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Governorate;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class GovernoratePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->hasPermission('view-coupons');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->hasPermission('add-coupons');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Governorate $governorate): bool
+    {
+        return $user->hasPermission('update-governorates');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Governorate $governorate): bool
+    {
+        return $user->hasPermission('delete-governorates');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Governorate $governorate): bool
+    {
+        return $user->hasPermission('delete-governorates');
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Governorate $governorate): bool
+    {
+        return $user->hasPermission('delete-governorates');
+    }
+}
