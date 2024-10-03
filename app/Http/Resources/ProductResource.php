8cde199cdf4d4e45b34287a8cb0f38db3fa48c59
Esraa_GamalH <esraa9gamal@gmail.com>
2024-10-03 23:12:31 +0200

    fix error: check hover_image not null

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -31,7 +31,7 @@ class ProductResource extends JsonResource
             'discount_type' => $discount->type,
             'discount_value' => $discount->value,
             'cover_image' => Str::startsWith($this->cover_image, 'http') ? $this->cover_image : asset("cover/{$this->cover_image}"),
-            'hover_image' => asset("images/{$this->images[0]->image_url}"),
+            'hover_image' => isset($this->images[0]) ? asset("images/{$this->images[0]->image_url}") : null,
             'colors' => $this->details->pluck('color')->unique(),
         ];
     }
