3fd49b5a80bafd00642e834153edc5f12682e7c9
Tarek Hesham <tarekghost6@gmail.com>
2024-10-05 20:36:33 +0300

    Cache for analytics

--- app/Http/Controllers/Control/AnalyticsController.php
+++ app/Http/Controllers/Control/AnalyticsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Control;
 use App\Http\Controllers\Controller;
 use App\Models\Orders\OrderItem;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class AnalyticsController extends Controller
 {
@@ -15,57 +16,49 @@ class AnalyticsController extends Controller
         $year = $request->get('year', date('Y'));
 
         // Get total sales
-        $salesData = OrderItem::with('productDetail')
-            ->whereYear('created_at', $year)
-            ->whereMonth('created_at', $month)
-            ->orderBy('created_at', 'asc')
-            ->get();
+        $cacheKey = "sales_data_{$year}_{$month}";
+        $salesData = Cache::remember($cacheKey, 60, function () use ($year, $month) {
+            return OrderItem::with(['productDetail' => function ($query) {
+                $query->select('id', 'product_id', 'color', 'size', 'material', 'stock', 'price');
+            }, 'order.orderCoupon.coupon' => function ($query) {
+                $query->select('id', 'coupon_code', 'discount_type', 'discount_value');
+            }])
+                ->whereYear('created_at', $year)
+                ->whereMonth('created_at', $month)
+                ->select('id', 'order_id', 'product_detail_id', 'quantity', 'total_price', 'created_at')
+                ->orderBy('created_at', 'asc')
+                ->get();
+        });
 
-        // Get total sales
         $totalSales = number_format($salesData->sum('total_price'), 2);
-        
-        // Get total items sold
         $totalItemsSold = $salesData->sum('quantity');
 
-        // Get total products sold
-        $totalProductsSold = $salesData->count();
-
-        // Get products sold
         $productsSold = $salesData->groupBy('product_detail_id')
-        ->map(function ($group) {
-            // Get product detail
-            $productDetail = $group->first()->productDetail;
+            ->map(function ($group) {
+                $productDetail = $group->first()->productDetail;
+                return [
+                    'product_id' => $productDetail->product_id,
+                    'name' => $productDetail->product->name,
+                    'color' => $productDetail->color,
+                    'size' => $productDetail->size,
+                    'material' => $productDetail->material,
+                    'in_stock' => $productDetail->stock,
+                    'product_price' => number_format($productDetail->price, 2),
+                    'total_quantity' => $group->sum('quantity'),
+                    'total_price' => number_format($group->sum('total_price'), 2),
+                    'details' => $group->map(function ($item) {
+                        return [
+                            'order_id' => $item->order_id,
+                            'quantity' => $item->quantity,
+                            'total_price' => number_format($item->total_price, 2),
+                            'created_at' => $item->created_at,
+                            'used_coupon' => $item?->order?->orderCoupon?->coupon ?? 'No coupon used',
+                        ];
+                    }),
+                ];
+            })->values();
 
-            return [
-                'product_id' => $productDetail->product_id,
-                'name' => $productDetail->product->name,
-                'color' => $productDetail->color,
-                'size' => $productDetail->size,
-                'material' => $productDetail->material,
-                'in_stock' => $productDetail->stock,
-                'product_price' => number_format($productDetail->price, 2),
-
-                'total_quantity' => $group->sum('quantity'),
-                'total_price' => number_format($group->sum('total_price'), 2),
-
-                'details' => $group->map(function ($item) {
-                    // Get used coupon
-                    $usedCoupon = $item?->order?->orderCoupon?->coupon;
-
-                    return [
-                        'order_id' => $item->order_id,
-                        'quantity' => $item->quantity,
-                        'total_price' => number_format($item->total_price, 2),
-                        'created_at' => $item->created_at,
-                        'used_coupon' => $usedCoupon ? [
-                            'coupon_code' => $usedCoupon?->coupon_code,
-                            'discount_type' => $usedCoupon?->discount_type,
-                            'discount_value' => $usedCoupon?->discount_value,
-                        ] : 'No coupon used',
-                    ];
-                }),
-            ];
-        })->values();
+        $totalProductsSold = $productsSold->count();
 
         return response()->json(compact('productsSold', 'totalItemsSold', 'totalProductsSold', 'totalSales'), 200);
     }
