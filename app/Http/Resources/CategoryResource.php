5ee05eb0ce8baf1dbeb1cbe0de9fddf071fb95ca
yossef <yossefmagdy78@gmail.com>
2024-10-08 19:03:03 +0300

    fix category and subcategory image prefix

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -18,7 +18,7 @@ class CategoryResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'image' =>  $image ? asset("cover/{$image}") : asset('default.png'),
+            'image' =>  $image ? asset("cover/{$image}") : asset('cover/default.png'),
         ];
     }
 }
