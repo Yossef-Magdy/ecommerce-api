96a68e945a92eb83026140190186b826d9d77e7d
Tarek Hesham <tarekghost6@gmail.com>
2024-10-09 17:15:20 +0300

    Enhancement performance Best Sellers

--- app/Http/Resources/ProductDetailsResource.php
+++ app/Http/Resources/ProductDetailsResource.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Resources;
 
-use App\Models\Products\ProductDiscount;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Str;
 
 class ProductDetailsResource extends JsonResource
 {
