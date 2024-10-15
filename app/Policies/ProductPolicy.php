964c9aa81e08d46664d52f40fdbf43557a3a012a
yossef <yossefmagdy78@gmail.com>
2024-10-15 19:33:53 +0300

    update product policy

--- app/Policies/ProductPolicy.php
+++ app/Policies/ProductPolicy.php
@@ -7,6 +7,22 @@ use App\Models\User;
 
 class ProductPolicy
 {
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->hasPermission('view-products');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Product $product): bool
+    {
+        return $user->hasPermission('view-products');
+    }
     /**
      * Determine whether the user can create models.
      */
