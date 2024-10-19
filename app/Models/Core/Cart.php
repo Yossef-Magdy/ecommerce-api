dcd5d509c966d0f4709f096ea5264bc401041176
Tarek Hesham <tarekghost6@gmail.com>
2024-10-20 01:44:45 +0300

    Initialize Cart controller and module

---
+++ app/Models/Core/Cart.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models\Core;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'items'];
+
+    protected $casts = [
+        'items' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
