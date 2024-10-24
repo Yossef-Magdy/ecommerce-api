ec0722ecdba0f12a101dc7c20f7ddb3b839338fe
yossef <yossefmagdy78@gmail.com>
2024-10-24 10:53:30 +0300

    update collection controller

--- app/Http/Controllers/Api/CollectionController.php
+++ app/Http/Controllers/Api/CollectionController.php
@@ -15,42 +15,11 @@ class CollectionController extends Controller
     public function index(Request $request)
     {
         $category = $request['category'];
-
         $products = Product::whereHas('categories', function ($query) use ($category) {
             $query->where('name', $category);
-        })
-            ->orWhereHas('subcategories', function ($query) use ($category) {
-                $query->where('name', $category);
-            })
-            ->distinct()
-            ->paginate(10);
-            
-        $response = [
-            // 'current_page' => $products->currentPage(),
-            'data' => ProductResource::collection($products),
-            // 'first_page_url' => $products->url(1),
-            // 'from' => $products->firstItem(),
-            // 'last_page' => $products->lastPage(),
-            // 'last_page_url' => $products->url($products->lastPage()),
-            // 'links' => [
-            //     [
-            //         'url' => $products->previousPageUrl(),
-            //         'label' => '&laquo; Previous',
-            //         'active' => !$products->onFirstPage(),
-            //     ],
-            //     [
-            //         'url' => $products->nextPageUrl(),
-            //         'label' => 'Next &raquo;',
-            //         'active' => $products->hasMorePages(),
-            //     ],
-            // ],
-            'next_page_url' => $products->nextPageUrl(),
-            // 'path' => $products->path(),
-            // 'per_page' => $products->perPage(),
-            // 'prev_page_url' => $products->previousPageUrl(),
-            // 'to' => $products->lastItem(),
-            // 'total' => $products->total(),
-        ];
-        return response()->json($response);
+        })->orWhereHas('subcategories', function ($query) use ($category) {
+            $query->where('name', $category);
+        })->distinct()->paginate(10);
+        return ProductResource::collection($products);
     }
 }
